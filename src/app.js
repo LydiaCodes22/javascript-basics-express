e6615bb6e338dda1ffe7591b4ff797ed454bcd2a
Lydia Wallace <lydia.wallace@outlook.com>
2022-04-22 12:14:58 +0100

    progress on the strings section

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
+/* eslint-disable prefer-destructuring */
 const express = require('express');
+const { restart } = require('nodemon');
 
 const app = express();
+
+app.get('/strings/hello/:string', (req, res) => {
+  // eslint-disable-next-line prefer-destructuring
+  const string = req.params.string;
+  const result = { result: `Hello, ${string}!` };
+  res.status(200).json(result);
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  const string = req.params.string;
+  const result = { result: `${string.toUpperCase()}` };
+  res.status(200).json(result);
+});
 
 module.exports = app;
